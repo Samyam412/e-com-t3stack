968185478784d90f5eff1bba6871e49962e1a2a8
Samyam412 <kcsamy50@gmail.com>
2024-04-16 23:10:56 +0545

    Add top navigation bar and mock images to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+const mockUrl = [
+  "https://utfs.io/f/a9d21af8-c8c7-4e6a-bde5-485f9bac3a2e-1d.1.jpg",
+  "https://utfs.io/f/c892e108-492e-4d00-bc4f-a668116a6a08-1f.jpg",
+  "https://utfs.io/f/8378a3d3-48b5-49a3-b636-0f5fba054bd8-1g.jpg",
+  "https://utfs.io/f/e89dc87d-0895-40cd-96b5-73882912c5a5-1e.jpg",
+];
 
+const mockImages = mockUrl.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Gallery
+    <main className="">
+      <div className="flex flex-wrap gap-4  ">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
+          (image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} />
+            </div>
+          ),
+        )}
+      </div>
     </main>
   );
 }
