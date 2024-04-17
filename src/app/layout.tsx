f5d0582d61ea7f46741a1ca80d560d3bd15ca3c6
Samyam412 <kcsamy50@gmail.com>
2024-04-17 21:00:53 +0545

    Update layout.tsx to include NextSSRPlugin and extractRouterConfig

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "@uploadthing/react/styles.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TopNav } from "./_components/topnav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,6 +27,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <body className={`font-sans ${inter.variable}`}>
           <TopNav />
           {children}
