72e24844f7425ed2f73262b60f0355ba46f4c6fd
Samyam412 <kcsamy50@gmail.com>
2024-04-17 14:03:36 +0545

    Add images domain to next.config.js and update homepage to display images based on user authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,42 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
   return (
+    <div className="flex flex-wrap gap-4  ">
+      {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="flex h-48 w-48 flex-col ">
+          <Image
+            className="object-cover"
+            height={192}
+            width={192}
+            alt={image.name}
+            src={image.url}
+          />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4  ">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col ">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="size-full text-2xl ">
+          Please sign in to see the images
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
     </main>
   );
 }
