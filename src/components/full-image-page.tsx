c55bc93454829795d7dd44f906153354508b9210
Samyam412 <kcsamy50@gmail.com>
2024-04-19 17:19:23 +0545

    Update image layout and styles in modal.tsx and full-image-page.tsx

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,11 +1,19 @@
 import { getImage } from "~/server/queries";
 
-export default async function FullPageImageView(props: {id: number}) {
- 
+export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
   return (
-    <div>
-        <img src={image.url} alt={image.name} className="h-96 w-96" />
+    <div className="flex size-full min-w-0 ">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="w-96  object-contain"
+        />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
     </div>
   );
 }
